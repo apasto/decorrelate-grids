ae74c96586fbde6c0678c2bf356487aef1d36ec1
Alberto Pastorutti <6527012+apasto@users.noreply.github.com>
2023-08-29 22:53:53 +0000

    refactor: method extraction, separate script-call in main()

--- linreg-grids.py
+++ linreg-grids.py
@@ -75,45 +75,408 @@ def wrap_linregress(a, b, e_i, hw_y_i, hw_x_i):
     return c0, c1, rv, pv, c0_stderr, c1_stderr, w_no_nan_count
 
 
-if len(valid_elements_idx) < n_processes:
-    n_processes = len(valid_elements_idx)
+def regression(A, B, window_halfwidth_x=None, window_halfwidth_y=None,
+               edge_width_x_i=0, edge_width_y_i=0,
+               global_flag=False, out_history=None, small_scale_n=None, n_processes=1,
+               **kwargs):
+    """
+    Call the regression, provided with the two dataarrays, windowing and edge parameters, number of processes.
+    """
 
-if n_processes == 1:
-    for element in valid_elements_idx:
-        (
-            out["c0"]["c0"][element[0], element[1]],
-            out["c1"]["c1"][element[0], element[1]],
-            out["rv"]["rv"][element[0], element[1]],
-            out["pv"]["pv"][element[0], element[1]],
-            out["ie"]["ie"][element[0], element[1]],
-            out["se"]["se"][element[0], element[1]],
-            out["np"]["np"][element[0], element[1]]) = wrap_linregress(
-                a=A, b=B, e_i=element,
-                hw_x_i=window_halfwidth_x_i,
-                hw_y_i=window_halfwidth_y_i)
-else:
-    pool = mp.Pool(n_processes)
-    for element in valid_elements_idx:
-        (
-            out["c0"]["c0"][element[0], element[1]],
-            out["c1"]["c1"][element[0], element[1]],
-            out["rv"]["rv"][element[0], element[1]],
-            out["pv"]["pv"][element[0], element[1]],
-            out["ie"]["ie"][element[0], element[1]],
-            out["se"]["se"][element[0], element[1]],
-            out["np"]["np"][element[0], element[1]]) = pool.apply(
-                wrap_linregress,
-                kwds={
-                    'a': A, 'b': B, 'e_i': element,
-                    'hw_x_i': window_halfwidth_x_i,
-                    'hw_y_i': window_halfwidth_y_i})
+    if window_halfwidth_x is None:
+        raise ValueError()
+    if window_halfwidth_y is None:
+        window_halfwidth_y = window_halfwidth_x
 
-# window_no_nan_ratio
-out["nr"]["nr"] = out["np"]["np"] / window_size
+    # assert for same sampling on A, B grids
+    if not np.all(np.isclose(A.x.values, B.x.values)):
+        raise ValueError("Sampling along x in A, B grids is not equal")
+    if not np.all(np.isclose(A.y.values, B.y.values)):
+        raise ValueError("Sampling along y in A, B grids is not equal")
 
-# save results (coefficients, quality) to grids
-for key in out.keys():
-    out[key].to_netcdf(
-        path=out_filename[key],
-        format="NETCDF4",
-        encoding={key: {"zlib": True, "complevel": 9}})
+    # grid intervals: assert for (close) equality of min and max interval
+    # testing grid A is enough, since we already tested for 'same sampled coords'
+
+    A_x_int = np.diff(A.x.values)
+    A_x_int_range = (A_x_int.min(), A_x_int.max())
+    if not np.isclose(A_x_int_range[0], A_x_int_range[1]):
+        raise ValueError("Grid sampling along x is not constant")
+
+    A_y_int = np.diff(A.y.values)
+    A_y_int_range = (A_y_int.min(), A_y_int.max())
+    if not np.isclose(A_y_int_range[0], A_y_int_range[1]):
+        raise ValueError("Grid sampling along y is not constant")
+
+    # global regression: not implemented yet
+    if global_flag:
+        raise NotImplementedError("Global regression is not implemented.")
+        # TODO: implement global regression
+
+    # store grid intervals
+    x_int = A_x_int_range[0]
+    y_int = A_y_int_range[0]
+
+    # dict of xr.DataArray for each of regression results
+    # same shape as data in A
+    # NaN-filled -> not assigned nodes will stay NaN
+    # TODO: use a single dataset with all the output variables
+    # it can be then used in gmt using the filename?variable notation
+    out_titles = {
+        "c0": "Regression intercept",
+        "c1": "Regression slope",
+        "rv": "r-value of regression",
+        "pv": "p-value of regression",
+        "se": "Standard error of slope",
+        "ie": "Standard error of intercept",
+        "np": "Number of non-nan points in rolling regression window",
+        "nr": "Ratio of non-nan points and total number of points in window",
+        }
+    out = {}
+    for key, value in out_titles.items():
+        out[key] = xr.Dataset(
+            data_vars={key: (["y", "x"], np.full(A.shape, np.nan))},
+            coords=dict(
+                x=(["x"], A.x.values),
+                y=(["y"], A.y.values),
+            ),
+            attrs={"title": value, "history": out_history},
+        )
+
+    # window half width from units to number-of-nodes
+    window_halfwidth_x_i = round(window_halfwidth_x / x_int)
+    window_halfwidth_y_i = round(window_halfwidth_y / y_int)
+
+    # number of grid elements to be discarded at the edges
+    edge_fullwidth_x_i = edge_width_x_i + window_halfwidth_x_i
+    edge_fullwidth_y_i = edge_width_y_i + window_halfwidth_y_i
+
+    # create a "discard this element mask"
+    # mask nan elements in A or B
+    discard = np.logical_or(np.isnan(A), np.isnan(B))
+    # mask edge elements
+    # x, left edge
+    discard[:, 0:edge_fullwidth_x_i] = True
+    # x, right edge
+    discard[:, -edge_fullwidth_x_i:] = True
+    # y, bottom edge
+    discard[0:edge_fullwidth_y_i, :] = True
+    # y, top edge
+    discard[-edge_fullwidth_y_i:, :] = True
+
+    # get indices in a (m, n) fashion of not-to-be-discarded points
+    # to center each rolling linreg on
+    valid_elements_idx = np.argwhere(np.logical_not(discard.values))
+
+    if small_scale_n is not None:
+        valid_elements_idx = valid_elements_idx[:small_scale_n, :]
+
+    # number of elements in window, used for ratio of non-NaN samples in window
+    window_size = (window_halfwidth_x_i * 2 + 1) * (window_halfwidth_y_i * 2 + 1)
+
+    # avoid more proccesses than window centers
+    if len(valid_elements_idx) < n_processes:
+        n_processes = len(valid_elements_idx)
+
+    if n_processes == 1:
+        for element in valid_elements_idx:
+            (
+                out["c0"]["c0"][element[0], element[1]],
+                out["c1"]["c1"][element[0], element[1]],
+                out["rv"]["rv"][element[0], element[1]],
+                out["pv"]["pv"][element[0], element[1]],
+                out["ie"]["ie"][element[0], element[1]],
+                out["se"]["se"][element[0], element[1]],
+                out["np"]["np"][element[0], element[1]]) = wrap_linregress(
+                    a=A, b=B, e_i=element,
+                    hw_x_i=window_halfwidth_x_i,
+                    hw_y_i=window_halfwidth_y_i)
+    else:
+        pool = mp.Pool(n_processes)
+        for element in valid_elements_idx:
+            (
+                out["c0"]["c0"][element[0], element[1]],
+                out["c1"]["c1"][element[0], element[1]],
+                out["rv"]["rv"][element[0], element[1]],
+                out["pv"]["pv"][element[0], element[1]],
+                out["ie"]["ie"][element[0], element[1]],
+                out["se"]["se"][element[0], element[1]],
+                out["np"]["np"][element[0], element[1]]) = pool.apply(
+                    wrap_linregress,
+                    kwds={
+                        'a': A, 'b': B, 'e_i': element,
+                        'hw_x_i': window_halfwidth_x_i,
+                        'hw_y_i': window_halfwidth_y_i})
+
+    # window_no_nan_ratio
+    out["nr"]["nr"] = out["np"]["np"] / window_size
+
+    return out
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Compute a rolling-window linear regression on 2 grids. "
+        + "The variable in grid A is assumed to be, at least partially, "
+        + "in a linear relationship with grid B. "
+        + "A linear regression is performed in boxcar-shaped rolling windows, "
+        + "of given size, avoding nodes which are closer to edges less than "
+        + "an half window, plus (optional) an edge of given width. "
+        + "Set either a 1:1 window half size, both x and y window half sizes, or "
+        + "the x/y aspect ratio of window sizes. "
+        + "The c0, c1 coefficients of a relationship in the form "
+        + "A = c0 + c1 * B are returned (as a grid) for each point "
+        + "in the input grids (which must cover the same region, "
+        + " with the same x, y intervals). "
+        + "Other returned grids are: rvalue, pvalue, c1 std error, c0 std error "
+        + "(see documentation of scipy.stats.linregress), "
+        + "the number of non-nan points in each window and the non-nan/total points ratio. "
+        + "All the returned grids have the same size and interval of grids A, B. "
+        + "The default form of the output grid files is: {path and stem of file A}_linreg_c0.grd "
+        + "(e.g. for c0)."
+    )
+
+    # arg_defs keys: argument name, values: kwargs to be passed to add_argument
+    arg_defs = {
+        # required: filename of grids A, B
+        "A_filename": {
+            "metavar": "A.grd",
+            "type": str,
+            "help": "filename of dependent variable grid A, (A = f(B))"
+            },
+        "B_filename": {
+            "metavar": "B.grd",
+            "type": str,
+            "help": "filename of independent variable grid B"
+            },
+        # required: window half size along x
+        "window_halfwidth_x": {
+            "metavar": "HALF_SIZE",
+            "type": float,
+            "help": "half size of window, in grid units, along x"
+            },
+        # window half size along y
+        "--window_halfwidth_y": {
+            "metavar": "HALF_SIZE_Y",
+            "type": float,
+            "default": None,
+            "help": "half size of window, in grid units, along y "
+            + "(set either this, aspect ratio, or neither (implies 1:1 aspect ratio)"
+            },
+        # window size x/y aspect ratio
+        "--window_aspect_ratio": {
+            "metavar": "ASPECT_RATIO",
+            "type": float,
+            "default": None,
+            "help": "x/y aspect ratio of window size"
+            + "(set either this, half size along y, or neither (implies 1:1 aspect ratio))"
+            },
+        # edge widths
+        "--edge_width_x_i": {
+            "metavar": "EDGE_WIDTH",
+            "type": int,
+            "default": 0,
+            "help": "number of grid nodes to be discarded at left and right edges, along x "
+            + "(default: 0)"
+            },
+        "--edge_width_y_i": {
+            "metavar": "EDGE_WIDTH_Y",
+            "type": int,
+            "default": 0,
+            "help": "number of grid nodes to be discarded at top and bottom edges, along y "
+            + " (default: 0)"
+            },
+        # cutoff min point per window: if less, no regression
+        "--minpoints": {
+            "metavar": "MIN_POINTS",
+            "type": int,
+            "default": 2,
+            "help": "do not perform regression on node if the rolling window "
+            + "contains less than MIN_POINTS non-NaN points"
+            },
+        # switch: parameter metadata in output filename(s)
+        "--metanames": {
+            "action": "store_true",
+            "help": "include the x and y half-window size (in grid units) "
+            + "in the output filenames, in the form "
+            + "'(...)linreg_x{windowsize_x}_y{windowsize_y}_(...)'. "
+            + "If the aspect ratio is provided, then the output is in the form: "
+            + "(...)linreg_x{windowsize_x}_(...)'."
+            },
+        # switch: global regression
+        "--global_flag": {
+            "action": "store_true",
+            "help": "instead of rolling regression, fit one regression "
+            + "to the entire region (except edges)"
+            },
+        # small scale run on n elements
+        "--small_scale_n": {
+            "metavar": "SMALL_SCALE_N",
+            "type": int,
+            "default": None,
+            "help": "run the regression only on the first N valid window centers"
+            + "(use case: small scale test run)"
+            },
+        # n processes (parallel with multiprocessing)
+        "--n_processes": {
+            "metavar": "N",
+            "type": int,
+            "default": 1,
+            "help": "number of processes for parallel run "
+            + "(1 : serial, <1 : use all logical CPU cores)"
+            },
+        # output grids in multiple nc files
+        "--split_out_files": {
+            "action": "store_true",
+            "help": "save each output grid in a separate nc file (default: as fields in a single file)"
+            },
+        # output path (defaults to same as A grid path)
+        "--output_path": {
+            "metavar": "OUT_PATH",
+            "type": str,
+            "default": None,
+            "help": "path to save the output file(s) in (default: same directory as A grid)"
+            },
+        # output stuff
+        "--out_basename_prefix" : {
+            "metavar": "OUT_PREFIX",
+            "type": str,
+            "default": None,
+            "help": "prepend this to the name of output file(s) (default: A grid filename, with no extension)"
+            }
+        }
+
+    for key, value in arg_defs.items():
+        parser.add_argument(
+            key,
+            **value
+        )
+
+    # parse arguments, to dict
+    args = vars(parser.parse_args())
+
+    return args
+
+
+def record_arguments():
+    """
+    concatenate how the script was called (script name and arguments)
+    this will be stored in the 'history' attribute of the output files
+    (gmt-like behaviour)
+    shlex is used to escape single quotes, if there are any
+    """
+    return " ".join(shlex.quote(arg) if " " in arg else arg for arg in sys.argv)
+
+
+def window_halfwidths_from_args(
+        window_halfwidth_x, window_halfwidth_y=None, window_aspect_ratio=None, **kwargs):
+    """
+    Manage the rolling window halfwidths provided in arguments, with the following cases:
+     - window_aspect_ratio provided, window_halfwidth_y computed accordingly
+     - window_halfwidth_y provided
+     - only window_halfwidth_x is provided, implies 1:1 aspect ratio
+    """
+    # x/y windows size or aspect ratio
+    if window_halfwidth_y is not None and window_aspect_ratio is not None:
+        # both provided: raise error
+        raise ValueError(
+            "Clashing parameters: "
+            + "both window half width along y and window size aspect ratio provided. "
+            + "Provide either the window size aspect ratio, size along y, "
+            + "or neither (implies 1:1 aspect ratio)."
+        )
+    elif window_halfwidth_y is None and window_aspect_ratio is None:
+        # neither provided: aspect ratio is 1:1
+        window_halfwidth_y = window_halfwidth_x
+        window_aspect_ratio = 1  # unused at the moment, keep record of it
+    elif window_aspect_ratio is not None:
+        window_halfwidth_y = window_halfwidth_x / window_aspect_ratio
+    return window_halfwidth_x, window_halfwidth_y, window_aspect_ratio
+
+
+def define_out_filenames(out_basename_prefix, out_dirname, windows_halfwidths_str, out_keys):
+    """
+    Define the output filenames, in a script call.
+    Populates a dict of filenames as: dirname / prefix + '_linreg' + halfwidths_str + key + '.'grd'
+    provided with a prefix, dirname, a string documenting the window size (may be empty).
+    Iterates on all the provided 'out_keys', suitable both for 'one file' and multiple file output.
+    """
+    out_filename = {
+        key: out_dirname / (out_basename_prefix + "_linreg_" + windows_halfwidths_str + str(key) + ".grd")
+        for key in out_keys
+    }
+    return out_filename
+
+
+def save_results(out, out_filename, zlib=True, complevel=9):
+    """
+    TODO: docstring
+    save results (coefficients, quality) to grids
+    """
+    for key in out.keys():
+        out[key].to_netcdf(
+            path=out_filename[key],
+            format="NETCDF4",
+            encoding={key: {"zlib": zlib, "complevel": complevel}})
+
+
+def main():
+    parsed_args = parse_arguments()
+    # manage window halfwidths arguments
+    (
+        parsed_args["window_halfwidth_x"],
+        parsed_args["window_halfwidth_y"],
+        parsed_args["window_aspect_ratio"]) = window_halfwidths_from_args(**parsed_args)
+
+    if not parsed_args["split_out_files"]:
+        raise NotImplementedError()  # TODO: implement one-file output
+
+    # window half widths as string in filenames
+    if parsed_args["metanames"]:
+        if parsed_args["global_flag"]:
+            windows_halfwidths_str = "GLOBAL_"
+        elif parsed_args["window_aspect_ratio"] is None:
+            # x and y sizes
+            windows_halfwidths_str = "x{:d}_y{:d}_".format(
+                round(parsed_args["window_halfwidth_x"]), round(parsed_args["window_halfwidth_y"]))
+        else:
+            # only x size
+            windows_halfwidths_str = "x{:d}_".format(
+                round(parsed_args["window_halfwidth_x"]))
+    else:
+        windows_halfwidths_str = ""
+
+    # output path for output grids
+    if parsed_args["output_path"] is None:
+        # default to path to A
+        out_dirname = parsed_args["A_filename"].resolve().parents[0]
+    else:
+        out_dirname = Path(parsed_args["output_path"])
+    if not out_dirname.exists():
+        raise FileNotFoundError(
+            "Provided output path: \"{:s}\" not found.".format(str(out_dirname)))
+
+    # history field for output grids: record arguments in call
+    out_history = record_arguments()
+
+    # using 'load' instead of 'open'
+    # this is an application-specific choice:
+    # usually InSAR unwrap/dem grids are relatively small
+    # resulting in no need for lazy-loading
+    A = xr.load_dataarray(parsed_args["A_filename"])
+    B = xr.load_dataarray(parsed_args["B_filename"])
+
+    # linreg call here
+    out = regression(A=A, B=B,
+                     out_history=out_history, **parsed_args)
+
+    # default prefix of output filenames: stem of A grid
+    if parsed_args["out_basename_prefix"] is None:
+        parsed_args["out_basename_prefix"] = Path(parsed_args["A_filename"]).stem
+    out_filename = define_out_filenames(parsed_args["out_basename_prefix"], out_dirname,
+                                        windows_halfwidths_str, out.keys())
+    save_results(out, out_filename)
+    return None
+
+
+if __name__ == "__main__":
+    main()
